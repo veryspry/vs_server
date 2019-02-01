85a5f126b587bcf669a4d27b6386aad271ce6f42
veryspry <ehlinger.matt@gmail.com>
2019-02-01 12:15:48 -0600

    set up to serve React app

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
+// Packages
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const session = require("express-session");
+const morgan = require("morgan");
+const path = require("path");
+// variables
 const port = 8080;
-
+// Components
 const router = require("./routes/index");
 
+// Set up middleware
+app.use(morgan("dev"));
+app.use(session({ secret: "app-secret" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(router);
+app.use(express.static(`${__dirname}/../ui/build`));
 
-app.get("/", (req, res) => {
-  res.send("api");
-});
-
+// Default Routes
 app.get("*", (req, res) => {
-  res.status(404);
-  res.send("404: not found");
+  res.sendFile(path.join(__dirname, "../", "ui/build/index.html"));
 });
 
+// Listen
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
