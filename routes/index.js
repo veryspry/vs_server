acb7e09dda6fe8a4a9f6a28c407bd64cf0fb221a
veryspry <ehlinger.matt@gmail.com>
2019-02-14 11:57:02 -0600

    add contact form endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const nodemailer = require("nodemailer");
+const titleize = require("underscore.string/titleize");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "veryspry.email@gmail.com",
+    pass: process.env.GOOGLE_APP_PASSWORD
+  }
+});
 
 router.post("/auth", (req, res) => {
   res.end("request received");
+});
+
+router.post("/contact-form", (req, res) => {
+  const defaultEmail = "veryspry.email@gmail.com";
+  const { name, email, message } = req.body;
+
+  let formattedName = titleize(name.trim());
+
+  if (formattedName.length === 0) formattedName = "Unidentified Person";
+
+  const mailOptions = {
+    from: email,
+    to: defaultEmail,
+    subject: `${formattedName}, contacted you at veryspry.com`,
+    html: message
+  };
+
+  transporter.sendMail(mailOptions, function(err, info) {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      console.log(info);
+      res.status(200).send(info);
+    }
+  });
 });
 
 module.exports = router;
